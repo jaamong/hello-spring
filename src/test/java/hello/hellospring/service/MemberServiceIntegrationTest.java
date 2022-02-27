2ca3c02b83cb23bf2b58fc113665271358768e36
jaamong <91050780+jaamong@users.noreply.github.com>
2022-02-27 14:26:22 +0900

    스프링 DB 접근 기술 (3)
    스프링 JdbcTemplate

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -2,9 +2,6 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,12 +27,11 @@ public class MemberServiceIntegrationTest {
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
 
-
     @Test
     void join() {
         // given : 뭔가 주어졌는데
         Member member = new Member();
-        member.setName("spring");
+        member.setName("hello");
 
         // when : 이거를 실행했을 때
         Long saveId = memberService.join(member);
@@ -58,7 +54,6 @@ public class MemberServiceIntegrationTest {
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
         assertThat(e, is(equalTo("이미 존재하는 회원입니다.")));
-
     }
 
 }
